c6d7e8ba00375888ebffbad0597c900efc420e70
KonvertPro <alfieharriswork@gmail.com>
2025-08-25 22:45:42 +0100

    Update main.jsx
    Added BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-
-import "@fontsource/poppins/400.css"  // Regular
-import "@fontsource/poppins/500.css"  // Medium
-import "@fontsource/poppins/600.css"  // Semibold
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/500.css'
+import '@fontsource/poppins/600.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter basename="/indraquestionnaire2">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
