d91072cdc50ae1995691a4ab387091f69e34d168
KonvertPro <alfieharriswork@gmial.com>
2025-08-25 12:28:58 +0100

    Deploy for GitHub Pages with correct base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NEW_REPO_NAME/', // <-- exactly your repo name, with leading & trailing slashes
+  base: '/Indraquestionnaire2/', // exact repo name, with leading & trailing slashes
 })
